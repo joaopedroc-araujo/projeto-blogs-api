e7fa8281469855c25c721f4eca2e54a03e9fdebc
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-09-27 16:26:43 -0300

    FEAT: cria model para user

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: DataTypes.INTEGER,
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+    }, {
+        underscored: true,
+    });
+    return User;
+};
+
+module.exports = UserModel;
