86d0897ac53535c216194c5642df0b80171d4ba2
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-09-27 16:01:39 -0300

    FEAT: cria migrations users, categories, blog_posts e posts_categories

---
+++ src/migrations/20230927182001-users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
